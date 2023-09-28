78621efbdad85f22eed63fecd6918c39cb718d0e
Poppe <djpo@users.noreply.github.com>
2023-09-28 12:32:59 +0200

    tone generated on click

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 // var canvas = document.getElementById("main");
 // var context = canvas.getContext("2d");
 
+// // ALT: load first, then resume playback when user interacts
+// var context;
+// window.onload = function () {
+//   context = new AudioContext();
+//   // set up all nodes
+//   // ...
+// };
+// // (then, in event listener)
+// context.resume().then(() => {
+//   console.log("Playback resumed successfully");
+// });
+
+let audioCtx = new AudioContext();
+const oscList = [];
+let mainGainNode = null;
+
+const wavePicker = document.querySelector("select[name='waveform']");
+const volumeControl = document.querySelector("input[name='volume']");
+
+// volumeControl.addEventListener("change", changeVolume, false);
+
+mainGainNode = audioCtx.createGain();
+mainGainNode.connect(audioCtx.destination);
+mainGainNode.gain.value = volumeControl.value;
+
 // listen to keyboard events
 document.addEventListener("keydown", function (e) {
   // left arrow key
@@ -21,25 +46,25 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+// listen to clicks on canvas
 document.querySelector("canvas").addEventListener("click", function () {
   console.log("click");
-
   try {
-    context = new AudioContext();
-    console.log("new AudioContext created successfully");
+    audioCtx.resume();
+
+    playTone(440);
   } catch (e) {
-    alert("Web Audio API is not supported in this browser");
+    console.log(e);
   }
 });
 
-// // ALT: load first, then resume playback when user interacts
-// var context;
-// window.onload = function () {
-//   context = new AudioContext();
-//   // set up all nodes
-//   // ...
-// };
-// // (then, in event listener)
-// context.resume().then(() => {
-//   console.log("Playback resumed successfully");
-// });
+function playTone(freq) {
+  const osc = audioCtx.createOscillator();
+  osc.connect(mainGainNode);
+
+  osc.type = wavePicker.options[wavePicker.selectedIndex].value;
+  osc.frequency.value = freq;
+  osc.start();
+
+  return osc;
+}
